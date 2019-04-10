a6ee1698a273b93bdb1498abaca0996f78e3f671
HJW-jdbc <wodnd1542@naver.com>
2019-04-10 13:56:00 +0900

    4/5과제

---
+++ src/main/java/org/HJW/book/chap08_1/MemberService.java
@@ -0,0 +1,45 @@
+package org.HJW.book.chap08_1;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.HJW.book.chap03.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * p.204 [리스트 8.13]를 Main과 Service로 분리한 Service 부분
+ * 
+ * @author Jacob
+ */
+@Service
+public class MemberService {
+
+	Logger logger = LogManager.getLogger();
+
+	@Autowired
+	MemberDao memberDao;
+
+
+	public void selectAll() {
+		List<Member> members = memberDao.selectAll(0, 100);
+		logger.debug(members);
+	}
+
+	public void updateMember() {
+		Member member = memberDao.selectByEmail("jacob@irafe.com");
+		member.setPassword("b");
+		memberDao.update(member);
+		logger.debug("Update complete.");
+	}
+
+	public void insertMember() {
+		Member member = new Member();
+		member.setEmail("wodnd1544@naver.com");
+		member.setPassword("a");
+		member.setName("한재웅");
+		memberDao.insert(member);
+		logger.debug("Insert complete.");
+	}
+}
